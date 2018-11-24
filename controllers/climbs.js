19f2b06460320b05ba8fafd8c024e29bac1ea91e
amoore16 <amoor16@gmail.com>
2018-11-24 00:42:31 -0700

    formatting

--- controllers/climbs.js
+++ controllers/climbs.js
@@ -4,29 +4,23 @@ const User = require('../models/user');
 module.exports = {
     index: async (req, res, next) => {
         const climbs = await Climb.find({});
-        
         res.status(200).json(climbs);
     },
-    
+
     getClimb: async (req, res, next) => {
         const climb = await Climb.findById(req.value.params.climbId);
-        
         res.status(200).json(climb);
     },
 
     newClimb: async (req, res, next) => {
         const user = await User.findById(req.value.body.user);
-        
         const newClimb = req.value.body;
         delete newClimb.user;
-
         const climb = new Climb(newClimb);
         climb.user = user;
         await climb.save();
-
         user.climbs.push(climb);
         await user.save();
-
         res.status(200).json(climb);
     },
 
@@ -34,32 +28,27 @@ module.exports = {
         const { climbId } = req.value.params;
         const newClimb = req.value.body;
         const result = await Climb.findByIdAndUpdate(climbId, newClimb);
-        
-        res.status(200).json({success: true});
+        res.status(200).json({ success: true });
     },
 
     updateClimb: async (req, res, next) => {
         const { climbId } = req.value.params;
         const newClimb = req.value.body;
         const result = await Climb.findByIdAndUpdate(climbId, newClimb);
-        
-        res.status(200).json({success: true});
+        res.status(200).json({ success: true });
     },
 
     deleteClimb: async (req, res, next) => {
         const { climbId } = req.value.params;
         const climb = await Climb.findById(climbId);
         if (!climb) {
-            return res.status(404).json({error: 'Climb doesn\'t exit'});
+            return res.status(404).json({ error: 'Climb doesn\'t exit' });
         }
         const userId = climb.user;
         const user = await User.findById(userId);
-
         await climb.remove();
-
         user.climbs.pull(climb);
         await user.save();
-        
         res.status(200).json({ success: true });
     }
 }
