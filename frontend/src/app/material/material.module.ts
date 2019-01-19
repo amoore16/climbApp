9cb230cff33525f415ffbb4d274e018f1612877f
amoore16 <amoor16@gmail.com>
2019-01-18 22:50:15 -0700

    material navbar

--- frontend/src/app/material/material.module.ts
+++ frontend/src/app/material/material.module.ts
@@ -2,16 +2,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 // Material Module Imports
-import {  }
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-
+    MatMenuModule,
+    MatButtonModule,
+    MatToolbarModule,
   ],
   exports: [
-    
+    MatMenuModule,
+    MatButtonModule,
+    MatToolbarModule
   ]
 })
 export class MaterialModule { }
