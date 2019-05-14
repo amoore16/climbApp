35328ac8f5ffc622eebeb85922618b2ac2d1eb60
alex moore <amoore16@gmail.com>
2019-05-14 10:07:51 -0600

    add climb form

--- frontend/src/app/components/climbs/add-climb/add-climb.component.ts
+++ frontend/src/app/components/climbs/add-climb/add-climb.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { BoulderDifficulty } from '../../../models/difficulty';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-add-climb',
@@ -8,7 +9,14 @@ import { BoulderDifficulty } from '../../../models/difficulty';
 })
 export class AddClimbComponent implements OnInit {
 
+  addClimbForm = new FormGroup({
+    routeType: new FormControl(''),
+    routeName: new FormControl(''),
+    routeDifficulty: new FormControl('')
+  });
+
   selectedValue: string;
+  diffSelect: BoulderDifficulty[];
 
   boulderDiff: BoulderDifficulty[] = [
     {value: 'v1', viewValue: 'V1'},
@@ -40,19 +48,50 @@ export class AddClimbComponent implements OnInit {
     {value: '5.13', viewValue: '5.13' },
     {value: '5.14', viewValue: '5.14' },
 
-  ]
+  ];
+
+  routeType: any[] = [
+    {value: 'top rope', viewValue: 'Top Rope'},
+    {value: 'sport', viewValue: 'Sport'},
+    {value: 'trad', viewValue: 'Trad'},
+    {value: 'boulder', viewValue: 'Boulder'}
+  ];
 
   constructor() { }
 
   ngOnInit() {
 
-    
+    this.diffSelect = this.standardDiff;
 
   }
   
   onSelect(event){
-    console.log('event', event);
-    console.log('hello');
+    // console.log('event', event);
+    
+    this.addClimbForm.get('routeDifficulty').reset;
+    
+    setTimeout(() => {
+      let value =  this.addClimbForm.get('routeDifficulty');    
+      console.log('value', value);
+      console.log('full form', this.addClimbForm.value.routeType);
+      if (this.addClimbForm.value.routeType === "boulder"){
+        this.diffSelect = this.boulderDiff;
+      } else {
+        this.diffSelect = this.standardDiff;
+      }
+    }, 200);
+    
+    // if (value === "boulder") {
+    //   this.addClimbForm.get('routeDifficulty').reset;
+    //   this.diffSelect = this.boulderDiff;
+    // } else {
+    //   this.diffSelect = this.standardDiff;
+    // }
+  }
+
+  onSubmit(){
+    console.log('hello')
+    console.log('climb form: ', this.addClimbForm.value);
   }
 
 }
