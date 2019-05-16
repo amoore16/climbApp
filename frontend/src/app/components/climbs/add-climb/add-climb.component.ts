1fe1f15426ec1b83406409a28c77f0685612cd47
alex moore <amoore16@gmail.com>
2019-05-16 09:52:49 -0600

    added difficulty models, fixed more alignment issues

--- frontend/src/app/components/climbs/add-climb/add-climb.component.ts
+++ frontend/src/app/components/climbs/add-climb/add-climb.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { BoulderDifficulty } from '../../../models/difficulty';
+import { RouteDifficulty } from '../../../models/difficulty';
+import { BoulderRatings } from '../../../models/boulderRatings';
+import { StandardRatings } from '../../../models/standardRatings';
 import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
@@ -16,39 +18,9 @@ export class AddClimbComponent implements OnInit {
   });
 
   selectedValue: string;
-  diffSelect: BoulderDifficulty[];
-
-  boulderDiff: BoulderDifficulty[] = [
-    {value: 'v1', viewValue: 'V1'},
-    {value: 'v2', viewValue: 'V2'},
-    {value: 'v3', viewValue: 'V3'},
-    {value: 'v4', viewValue: 'V4'},
-    {value: 'v5', viewValue: 'V5'},
-    {value: 'v6', viewValue: 'V6'},
-    {value: 'v7', viewValue: 'V7'},
-    {value: 'v8', viewValue: 'V8'},
-    {value: 'v9', viewValue: 'V9'},
-    {value: 'v10', viewValue: 'V10'},
-    {value: 'v11', viewValue: 'V11'},
-    {value: 'v12', viewValue: 'V12'},
-  ];
-
-  standardDiff: BoulderDifficulty[] = [
-    {value: '5.5', viewValue: '5.5' },
-    {value: '5.6', viewValue: '5.6' },
-    {value: '5.7', viewValue: '5.7' },
-    {value: '5.8', viewValue: '5.8' },
-    {value: '5.9', viewValue: '5.9' },
-    {value: '5.10a', viewValue: '5.10a' },
-    {value: '5.10b', viewValue: '5.10b' },
-    {value: '5.10c', viewValue: '5.10c' },
-    {value: '5.10d', viewValue: '5.10d' },
-    {value: '5.11', viewValue: '5.11' },
-    {value: '5.12', viewValue: '5.12' },
-    {value: '5.13', viewValue: '5.13' },
-    {value: '5.14', viewValue: '5.14' },
-
-  ];
+  diffSelect: RouteDifficulty[];
+  boulderDiff = BoulderRatings;
+  standardDiff = StandardRatings;
 
   routeType: any[] = [
     {value: 'top rope', viewValue: 'Top Rope'},
@@ -60,32 +32,21 @@ export class AddClimbComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-
     this.diffSelect = this.standardDiff;
-
   }
   
   onSelect(event){
-    // console.log('event', event);
-    
-    this.addClimbForm.get('routeDifficulty').reset;
-    
+    this.addClimbForm.get('routeDifficulty').reset();
     setTimeout(() => {
-      let value =  this.addClimbForm.get('routeDifficulty');    
-      console.log('value', value);
-      console.log('full form', this.addClimbForm.value.routeType);
       if (this.addClimbForm.value.routeType === "boulder"){
         this.diffSelect = this.boulderDiff;
       } else {
         this.diffSelect = this.standardDiff;
       }
-    }, 200);
-    
+    });
   }
 
   onSubmit(){
-    console.log('hello')
-    console.log('climb form: ', this.addClimbForm.value);
     this.addClimbForm.reset();
   }
 
---
+++ frontend/src/app/models/boulderRatings.ts
@@ -0,0 +1,16 @@
+import { RouteDifficulty } from './difficulty';
+
+export const BoulderRatings: RouteDifficulty[] = [
+    {value: 'v1', viewValue: 'V1'},
+    {value: 'v2', viewValue: 'V2'},
+    {value: 'v3', viewValue: 'V3'},
+    {value: 'v4', viewValue: 'V4'},
+    {value: 'v5', viewValue: 'V5'},
+    {value: 'v6', viewValue: 'V6'},
+    {value: 'v7', viewValue: 'V7'},
+    {value: 'v8', viewValue: 'V8'},
+    {value: 'v9', viewValue: 'V9'},
+    {value: 'v10', viewValue: 'V10'},
+    {value: 'v11', viewValue: 'V11'},
+    {value: 'v12', viewValue: 'V12'},
+];
