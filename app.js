08a763d6168d82bac8707232d02b16f0c154bb25
amoore16 <amoore16@gmail.com>
2020-02-22 14:43:17 -0700

    updated to dynamically set prt

--- app.js
+++ app.js
@@ -48,5 +48,7 @@ app.use((err, res, next) => {
 //error handler function
 
 // start server
-const port = app.get('port') || 8080;
+if (!process.env.PORT) { const env = require('./env.js'); }
+const port = process.env.PORT || 8080;
+
 app.listen(port, () => console.log(`Server is listening on port ${port}`));
